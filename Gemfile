0403b7a035533d862d13db0d47025b33713d2a26
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-06-24 10:52:28 +0600

    Working with pie chart

--- Gemfile
+++ Gemfile
@@ -45,3 +45,4 @@ end
 # gem 'debugger', group: [:development, :test]
 
 gem 'charter', :path => '/home/jitu/projects/charter'
+gem 'awesome_print'
--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,12 +2,17 @@ require 'charter/factory'
 
 class ChartsController < ApplicationController
   def index
-    @chart = Charter::Factory.build(:bar) do |chart|
+    @chart = Charter::Factory.build(:line) do |chart|
       chart.data = [{hours_remain: 100, estimated_hours_remain: 100, day: 3.days.ago.to_date},
                     {hours_remain: 50, estimated_hours_remain: 45, day: 2.days.ago.to_date},
                     {hours_remain: 5, estimated_hours_remain: 10, day: 1.days.ago.to_date}]
       chart.columns = {hours_remain: 'Hours remaining', estimated_hours_remain: 'Estimated hours remaining'}
       chart.label_column = :day
+    end
+
+    @pie_chart = Charter::Factory.build(:pie) do |chart|
+      chart.data = {'ruby' => 100,
+                    'python' => 12}
     end
   end
 end
