876d221f1c089fae8da940bc366471a9ddfc799e
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-07-08 15:24:10 +0600

    Updated to 0.2.2

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -2,7 +2,7 @@ require 'chartify/factory'
 
 class ChartsController < ApplicationController
   def index
-    @chart = Chartify::Factory.build(:line) do |chart|
+    @chart = Chartify::Factory.build(:area) do |chart|
       chart.data = [{hours_remain: 100, estimated_hours_remain: 100, day: 3.days.ago.to_date},
                     {hours_remain: 50, estimated_hours_remain: 45, day: 2.days.ago.to_date},
                     {hours_remain: 5, estimated_hours_remain: 10, day: 1.days.ago.to_date}]
@@ -13,7 +13,7 @@ class ChartsController < ApplicationController
     @pie_chart = Chartify::Factory.build(:pie) do |chart|
       chart.data = {'ruby' => 100,
                     'python' => 12}
-      chart.columns = ['Language', 'Usage']
+      # chart.columns = ['Language', 'Usage']
     end
   end
 end
